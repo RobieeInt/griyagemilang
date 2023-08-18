77dd68d6a6978ea8dc924e146b6e981a08e8ba5c
Robby Hernowo <robbyhernowo@gmail.com>
2023-08-18 15:39:23 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\ProductCategories;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory,SoftDeletes;
+    protected $fillable = [
+        'category_id',
+        'code',
+        'name',
+        'slug',
+        'price',
+        'old_price',
+        'tags',
+        'star',
+        'description',
+        'short_description',
+        'status',
+        'popular',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(ProductCategories::class);
+    }
+
+    //productGalleries
+    public function galleries()
+    {
+        return $this->hasMany(ProductGalleries::class)->where('status','active');
+    }
+
+
+
+}
---
+++ resources/views/components/banner2.blade.php
@@ -0,0 +1,40 @@
+<section class="banner__section section--padding color-scheme-2 pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="banner__section--inner d-flex align-items-center">
+                    <div class="banner__discount--content">
+                        <h3 class="banner__discount--content__subtitle">Get Discount Up to 80%</h3>
+                        <h2 class="banner__discount--content__title">Top Trending
+                            Furniture Items</h2>
+                        <p class="banner__discount--content__desc">Lorem ipsum dolor sit amet, consecteturiuyr
+                            icing elit,sed do eiusmod ut labore et.
+                            Lorem ipsum dolor sit amet, consecteturop.</p>
+                        <a class=" primary__btn btn__style2" href="shop.html" rel="nofollow">Discover
+                            More</a>
+                    </div>
+                    <div class="banner2__right--sidebar position__relative d-flex">
+                        <div class="banner2__sidebar--items one position__relative">
+                            <a class="banner__items--thumbnail " href="shop.html" rel="nofollow"><img
+                                    class="banner__items--thumbnail__img"
+                                    src="{{ asset('ggm/assets/img/banner/banner17.webp') }}" alt="banner-img"></a>
+                            <div class="product__badge style2">
+                                <span class="product__badge--items style2 sale">Chair</span>
+                            </div>
+                        </div>
+                        <div class="banner2__sidebar--items two position__relative">
+                            <a class="banner__items--thumbnail " href="shop.html" rel="nofollow"><img
+                                    class="banner__items--thumbnail__img"
+                                    src="{{ asset('ggm/assets/img/banner/banner16.webp') }}" alt="banner-img"></a>
+                        </div>
+                        <div class="banner2__sidebar--items three position__relative">
+                            <a class="banner__items--thumbnail " href="shop.html" rel="nofollow"><img
+                                    class="banner__items--thumbnail__img"
+                                    src="{{ asset('ggm/assets/img/banner/banner15.webp') }}" alt="banner-img"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
